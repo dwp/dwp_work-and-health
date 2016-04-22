6330765a1cf6b08bef641b6b3e9c9a25dbe81764
Paul Smith <mail@airfront.co.uk>
2016-04-22 07:19:42 +0100

    adds start route, re-arranges directory structure

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-  
+
   res.render('index');
 
 });
@@ -12,32 +12,11 @@ router.get('/', function (req, res) {
 
 // Passing data into a page
 
-router.get('/examples/template-data', function (req, res) {
-
-  res.render('examples/template-data', { 'name' : 'Foo' });
-
+router.get('/start', function (req, res) {
+  res.redirect('journal/about_me');
 });
 
 // Branching
-
-router.get('/examples/over-18', function (req, res) {
-
-  // get the answer from the query string (eg. ?over18=false)
-  var over18 = req.query.over18;
-
-  if (over18 == "false"){
-
-    // redirect to the relevant page
-    res.redirect("/examples/under-18");
-
-  } else {
-
-    // if over18 is any other value (or is missing) render the page requested
-    res.render('examples/over-18');
-
-  }
-
-});
 
 // add your routes here
 
