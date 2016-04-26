6d4b31ac548bb453de15cd130ec02b0a1086db4a
Paul Smith <mail@airfront.co.uk>
2016-04-26 19:55:51 +0100

    updates macros default data and adds form elements modifier to make full width textareas

--- app/views/journal/version-01/version_routes.js
+++ app/views/journal/version-01/version_routes.js
@@ -1,11 +1,9 @@
 module.exports = function(router, config) {
-  
+
   router.all(config.protoPaths.step, function(req,res,next){
 
     var requestedPage = req.params.step,
         postData = req.body || {};
-
-    console.log('test');
 
     next();
 
