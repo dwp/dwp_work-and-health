396bcdae3f7d5a558e8c34d80466cbb6e0f0fa9f
Paul Smith <mail@airfront.co.uk>
2016-04-26 18:51:43 +0100

    includes dependancy

--- app/utils.js
+++ app/utils.js
@@ -1,3 +1,5 @@
+var _ = require('lodash');
+
 exports.getVersionName = function(path) {
   var sp = path.split('/');
   var computedPath = _.join( _.slice( sp, ( _.indexOf(sp, 'views') +1 ) ), '/' );
